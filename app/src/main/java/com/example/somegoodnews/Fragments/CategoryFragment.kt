afc042509544fad1da4e6464b44dafb79ad85aef
rajoshich <rajoshich@gmail.com>
2020-06-07 08:24:37 +0530

    categories added

--- app/src/main/java/com/example/somegoodnews/Fragments/CategoryFragment.kt
+++ app/src/main/java/com/example/somegoodnews/Fragments/CategoryFragment.kt
@@ -1,18 +1,19 @@
 package com.example.somegoodnews.Fragments
 
-import android.content.Context
-import android.content.Intent
+import TabsAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.somegoodnews.R
-import com.example.somegoodnews.UserActivity
 import kotlinx.android.synthetic.main.fragment_category.*
 
+
 class CategoryFragment : Fragment() {
+    lateinit var pageAdapter: TabsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,8 +25,60 @@ class CategoryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        testSignIn.setOnClickListener {
-            startActivity(Intent(context, UserActivity::class.java))
+
+        category1.setOnClickListener {
+            val fragment = CategoryListFragment()
+            val args = Bundle()
+            args.putString("CATEGORY", "Humans being bros")
+            fragment.arguments = args
+
+            val fragmentManager: FragmentManager = activity!!.supportFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()        }
+
+        category2.setOnClickListener {
+            val fragment = CategoryListFragment()
+            val args = Bundle()
+            args.putString("CATEGORY", "Aww")
+            fragment.arguments = args
+
+            val fragmentManager: FragmentManager = activity!!.supportFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()        }
+
+        category3.setOnClickListener {
+            val fragment = CategoryListFragment()
+            val args = Bundle()
+            args.putString("CATEGORY", "Covid-19")
+            fragment.arguments = args
+
+            val fragmentManager: FragmentManager = activity!!.supportFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
         }
+
+        category4.setOnClickListener {
+            val fragment = CategoryListFragment()
+            val args = Bundle()
+            args.putString("CATEGORY", "Other")
+            fragment.arguments = args
+
+            val fragmentManager: FragmentManager = activity!!.supportFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+//            pageAdapter = TabsAdapter(fragmentManager)
+//            pageAdapter.addFragments(CategoryListFragment(), "Category list news")
+//            viewPager.adapter = pageAdapter
+//            tabLayout.setupWithViewPager(viewPager)
+//            pageAdapter.instantiateItem(viewPager, viewPager.currentItem) as NewsListFragment
     }
 }
---
+++ app/src/main/java/com/example/somegoodnews/Fragments/CategoryListFragment.kt
@@ -0,0 +1,66 @@
+package com.example.somegoodnews.Fragments
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.somegoodnews.Listeners.OnUpdateListListener
+import com.example.somegoodnews.Managers.ArticlesAdapter
+import com.example.somegoodnews.Managers.NewsArticle
+import com.example.somegoodnews.R
+import com.example.somegoodnews.SGNApp
+import kotlinx.android.synthetic.main.newslist_fragment.*
+
+class CategoryListFragment: Fragment() {
+    private lateinit var articlesAdapter: ArticlesAdapter
+    lateinit var app: SGNApp
+    private var onUpdateListListener: OnUpdateListListener? = null
+
+    private val categoryArticles: MutableList<NewsArticle> = mutableListOf()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        app = (context?.applicationContext as SGNApp)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is OnUpdateListListener) {
+            onUpdateListListener = context
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return layoutInflater.inflate(R.layout.newslist_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        testSignIn.setOnClickListener {
+//            startActivity(Intent(context, UserActivity::class.java))
+//        }
+         val category = arguments?.get("CATEGORY")
+        Log.i("poop2", category as String)
+        val allArticles = app.dataManager.articles
+        Log.i("poop", allArticles.toString())
+        for (article in allArticles) {
+            if (article.category == category) {
+                categoryArticles.add(article)
+            }
+        }
+
+        articlesAdapter = ArticlesAdapter(categoryArticles)
+        rvNewsList.adapter = articlesAdapter
+
+        articlesAdapter.onArticleLongClickListener = { newsArticle: NewsArticle, i: Int ->
+            app.likeArticle(newsArticle, i)
+        }
+    }
+}
+
